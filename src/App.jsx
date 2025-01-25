7112e9afb66d53c768f5e36d98b53475d18c9f47
masum362 <masumahmed64077@gmail.com>
2025-01-25 21:53:00 +0600

    initialize app and also setup environment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from '@/assets/Foodieland..png'
+function App() {
+ 
+
+  return (
+    <>
+    <h1>this is text</h1>
+      <img src={logo} alt="Foodieland..png" />
+    </>
+  )
+}
+
+export default App
